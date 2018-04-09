06b146bf0d4ff0764acbecb30e345cce301c6eaf
smilingkite <mail@katinkahesselink.net>
2018-04-09 14:30:29 +0200

    fix styles

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -9,7 +9,9 @@
       </b-form-textarea>
       <button class="button">submit</button>
     </form>
-    <pre class="mt-3">{{ tags }}</pre>
+    <ul class="tags">
+      <li v-for="tag in tags" :key="tag.id" v-bind:style="{fontSize: 1 + tag.count/5  + 'em'}" >{{tag.name}}</li>
+    </ul>
   </div>
 </template>
 
@@ -19,6 +21,11 @@ export default {
     return {
       text: '',
       tags: []  
+    }
+  },
+  computed: {
+    tagCount: function(count){
+      return 'tag' + count
     }
   },
   methods: {
@@ -62,24 +69,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+/* @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1"; */
 @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
-  .holder {
-    background: #fff;
-  }
+
   ul {
     margin: 0;
     padding: 0;
     list-style-type: none;
+    background-color: #E0EDF4;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: wrap;
   }
   
   ul li {
     padding: 20px;
     font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
+
     margin-bottom: 2px;
-    color: #3E5252;
+    display: inline-block;
   }
   p {
     text-align:center;
@@ -90,39 +100,12 @@ export default {
     box-shadow: 0px 0px 40px lightgray;
   }
   textarea {
-    width: calc(100% - 40px);
+    width: calc(100%);
     border: 0;
     padding: 20px;
     font-size: 1.3em;
     background-color: #323333;
     color: #687F7F;
   }
-  .alert {
-    background: #fdf2ce;
-    font-weight: bold;
-    display: inline-block;
-    padding: 5px;
-    margin-top: -20px;
-  }
-  .alert-in-enter-active {
-    animation: bounce-in .5s;
-  }
-  .alert-in-leave-active {
-    animation: bounce-in .5s reverse;
-  }
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.5);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-i {
-  float:right;
-  cursor:pointer;
-}
+
 </style>
