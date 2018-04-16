cd8f7e6ed4aa89276c2144af77cee5ccbe007b07
smilingkite <mail@katinkahesselink.net>
2018-04-16 09:19:24 +0200

    fix: show tags through VueX

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,46 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+function toTags (array) {
+  array.sort()
+  var tagArray = Object.values(array.reduce((resultTagArr, tag) => {
+    if (!resultTagArr[tag]) {
+      resultTagArr[tag] = {name: tag, count: 1}
+      // gives array of named objects: tagName: {name: tag, count: 1}
+    } else { resultTagArr[tag]['count'] += 1 }
+    return resultTagArr
+  }, {}))
+  return tagArray
+}
+function filterTags (tagArray) {
+  var commontags = ['', 
+  'a',  'an', 'and', 'as', 'are', 'at',
+  'but', 'be', 'by',
+  'don', 
+  'every',
+  'for', 
+  'had', 'has', 'have',
+  'is', 'in', 'it', 'if', 'i',
+  'like',
+  'me', 'my',
+  'of', 'on', 'or',
+  'so',
+  'the',  'that', 'this', 'to', 't', 'then',
+  'you', 'your',
+  'what', 'which', 'with', 'was', 'were', 'we'
+  ]
+  return tagArray.filter(tagObject => {
+    return !commontags.includes(tagObject.name)
+  })
+}
+function maxTags (tagArray, c = count) {
+  if (tagArray.length < 10) return tagArray
+  tagArray = tagArray.filter(tagObject => {
+    return tagObject.count > c 
+  })
+  return tagArray
+}
+
 export default new Vuex.Store({
  state: {
   text: '',
@@ -10,6 +50,10 @@ export default new Vuex.Store({
   count: 1, 
  },
  mutations: {
+  GET_TAGS (state, tags) {
+    tags = filterTags(toTags(tags))
+    state.tags = tags
+  },
   INCREMENT (state) {
     state.count++;
   },
@@ -20,6 +64,9 @@ export default new Vuex.Store({
   }
  },
  actions: {
+  gettags({commit}, tags){
+    commit('GET_TAGS', tags)
+  },
   increment({commit},count){
     commit('INCREMENT', count)
   },
@@ -28,7 +75,7 @@ export default new Vuex.Store({
   }
  },
  getters: {
-  //  tags: state => state.tags
+  tags: state => state.tags,
   count: state => state.count
  }
 
